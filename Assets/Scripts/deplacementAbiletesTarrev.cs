81df7014d262e1926a4fc5e286b002826a191ccc
William Foucher <e1938665@cmaisonneuve.qc.ca>
2022-01-22 21:29:30 -0500

    fix line renderer

--- Assets/Scripts/deplacementAbiletesTarrev.cs
+++ Assets/Scripts/deplacementAbiletesTarrev.cs
@@ -8,24 +8,24 @@ using System.Linq;
 
 public class deplacementAbiletesTarrev : MonoBehaviourPunCallbacks
 {
-    public Camera cameraJoueur; //Caméra du joueur
+    public Camera cameraJoueur; //Camï¿½ra du joueur
     public float vitesse = 7f; //Vitesse du personnage
     public float vitesseTourne; //Vitesse de rotation du personnage
     public float forceDuSaut; //Force du jump
-    public float gravite; //Gravité
-    private CharacterController controleur; //Référence au character controler
-    private float velocitePersoY; //Vélocité
+    public float gravite; //Gravitï¿½
+    private CharacterController controleur; //Rï¿½fï¿½rence au character controler
+    private float velocitePersoY; //Vï¿½locitï¿½
     //ATTAQUES
-    public static float degatAttaqueTarrev; //Dégâts de l'attaque de base de Tarrev
-    public float rayon; //Rayon maximum à l'entour de Tarrev
-    public GameObject particulePassif; //Particule du passi à tarrev
+    public static float degatAttaqueTarrev; //Dï¿½gï¿½ts de l'attaque de base de Tarrev
+    public float rayon; //Rayon maximum ï¿½ l'entour de Tarrev
+    public GameObject particulePassif; //Particule du passi ï¿½ tarrev
     List<float> distances = new List<float>(); //Liste des distances des objets proches
     List<GameObject> ennemisProches = new List<GameObject>(); //Liste des objets proches
     public float distancePlusProche; //Distance la plus proche lorsque le passif change
     public int indexPlusProche; //Index de l'objet le plus proche lorsque le passif change
     public float valeurPassifAConvertir; //Distance de la cible
     public float cooldownPassif = 20f; //Cooldown du passif
-    public bool verifierCible = true; //Permet de vérifier si on a changer de cible
+    public bool verifierCible = true; //Permet de vï¿½rifier si on a changer de cible
     public GameObject ciblePassif; //Cible du passif
     public LineRenderer LineRenderer;
 
@@ -34,21 +34,21 @@ public class deplacementAbiletesTarrev : MonoBehaviourPunCallbacks
         //Raccourci pour le character controler
         controleur = GetComponent<CharacterController>();
 
-        //Activer la caméra pour le joueur local seulement
+        //Activer la camï¿½ra pour le joueur local seulement
         if (photonView.IsMine)
         {
             cameraJoueur.gameObject.SetActive(true);
         }
 
-        //À chaque 20 secondes, Tarrev marque l’ennemi le plus proche.
+        //ï¿½ chaque 20 secondes, Tarrev marque lï¿½ennemi le plus proche.
         InvokeRepeating("marquePassif", 0f, cooldownPassif);
 
         LineRenderer = GameObject.FindGameObjectWithTag("ligne").GetComponent<LineRenderer>();
     }
 
     /*
-     * À chaque 30 secondes, Tarrev marque l’ennemi le plus proche. 
-     *   Tarrev gagne plus d’hauteur de saut et de plus en plus de damage dépendamment de la distance séparant sa marque de lui.
+     * ï¿½ chaque 30 secondes, Tarrev marque lï¿½ennemi le plus proche. 
+     *   Tarrev gagne plus dï¿½hauteur de saut et de plus en plus de damage dï¿½pendamment de la distance sï¿½parant sa marque de lui.
      * 
     */
 
@@ -56,19 +56,19 @@ public class deplacementAbiletesTarrev : MonoBehaviourPunCallbacks
     {
         if (photonView.IsMine && viePersonnage.mort == false)
         {
-            //Gère la rotation du joueur
+            //Gï¿½re la rotation du joueur
             float tourne = Input.GetAxis("Mouse X") * vitesseTourne * Time.deltaTime;
             transform.Rotate(0f, tourne, 0f);
 
-            //On récupère la valeur de l'axe de rotation
+            //On rï¿½cupï¿½re la valeur de l'axe de rotation
             Vector3 valeursInputs = Vector3.zero;
             valeursInputs.x = Input.GetAxisRaw("Horizontal");
             valeursInputs.z = Input.GetAxisRaw("Vertical");
 
-            //On converti un déplacement local en valeur selon les axes du monde
+            //On converti un dï¿½placement local en valeur selon les axes du monde
             Vector3 deplacement = transform.TransformDirection(valeursInputs * vitesse);
 
-            //Si le joueur est au sol et ne bouge pas, régler la vélocité à 0
+            //Si le joueur est au sol et ne bouge pas, rï¿½gler la vï¿½locitï¿½ ï¿½ 0
             if (controleur.isGrounded && velocitePersoY < 0)
             {
                 velocitePersoY = 0f;
@@ -92,7 +92,7 @@ public class deplacementAbiletesTarrev : MonoBehaviourPunCallbacks
                 vitesse = 10f;
             }
 
-            //On prend la velocite Y auquuel on ajoute la gravité
+            //On prend la velocite Y auquuel on ajoute la gravitï¿½
             velocitePersoY += gravite * Time.deltaTime;
             deplacement.y = velocitePersoY;
 
@@ -126,11 +126,11 @@ public class deplacementAbiletesTarrev : MonoBehaviourPunCallbacks
             LineRenderer.endWidth = 0.3f;
 
             // set the position
-            LineRenderer.SetPosition(0, cameraJoueur.transform.position);
+            LineRenderer.SetPosition(0, gameObject.transform.position);
             LineRenderer.SetPosition(1, ciblePassif.transform.position);
         }
 
-        //SI LA CIBLE A ÉTÉ TUÉE, CHANGER DE CIBLE
+        //SI LA CIBLE A ï¿½Tï¿½ TUï¿½E, CHANGER DE CIBLE
         if (ciblePassif.gameObject == null && verifierCible == false)
         {
             verifierCible = true;
@@ -148,7 +148,7 @@ public class deplacementAbiletesTarrev : MonoBehaviourPunCallbacks
         distances.Clear();
         ennemisProches.Clear();
 
-        //Pour chaque collider trouvé
+        //Pour chaque collider trouvï¿½
         foreach (Collider objetProche in colliders)
         {
             //Si il a un rigidbody
@@ -158,10 +158,10 @@ public class deplacementAbiletesTarrev : MonoBehaviourPunCallbacks
                 //Remplir une liste avec toutes les distances entre l'objet et Tarrev
                 float distance = Vector3.Distance(gameObject.transform.position, objetProche.gameObject.transform.position);
 
-                //Ajouter l'ennemi à la liste des ennemis proches
+                //Ajouter l'ennemi ï¿½ la liste des ennemis proches
                 ennemisProches.Add(objetProche.gameObject);
 
-                //Ajouter la distance à la liste des distances proches
+                //Ajouter la distance ï¿½ la liste des distances proches
                 distances.Add(distance);
             }  
         }
@@ -190,18 +190,18 @@ public class deplacementAbiletesTarrev : MonoBehaviourPunCallbacks
                 settingsParticule.startColor = new ParticleSystem.MinMaxGradient(Color.yellow);
             }
 
-            //La détruire après 20 secondes
+            //La dï¿½truire aprï¿½s 20 secondes
             StartCoroutine(enleverParticule(nouvelleParticulePassif.gameObject, cooldownPassif));
         }
     }
 
-    //Enlever la particule sur le réseau
+    //Enlever la particule sur le rï¿½seau
     public IEnumerator enleverParticule(GameObject particule, float delai)
     {
-        //Délai
+        //Dï¿½lai
         yield return new WaitForSeconds(delai);
 
-        //Détruire la particule si l'ennemi n'a pas été tué
+        //Dï¿½truire la particule si l'ennemi n'a pas ï¿½tï¿½ tuï¿½
         if (particule != null)
         {
             PhotonNetwork.Destroy(particule);
